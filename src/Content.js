18e9aff28671541d53f332869f6385062b5dfe90
giselle2323 <yakubuaminat94@gmail.com>
2020-04-21 03:28:59 +0100

    Use of Objects in React

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,9 @@ import Part from './Part'
 const Content = (props) => {
   return (
     <div>
-      <Part part = {props.part1} exercise = {props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part parts= {props.parts[0] } />
+      <Part parts= { props.parts[1] } />
+      <Part parts= { props.parts[2] } />
     </div>
   )
 };
